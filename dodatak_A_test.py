160429b4320d8691425536aadd5c24beaaa23af3
Nikola Kescec <nikola.kescec@fer.hr>
2022-05-25 20:43:16 +0200

    Init

---
+++ dodatak_A_test.py
@@ -0,0 +1,23 @@
+import math
+import unittest
+
+from numpy import NaN
+import dodatak_A
+
+class DodatakATest(unittest.TestCase):
+
+    def test_divide_with_zero(self):
+        operations_manager = dodatak_A.OperationsManager(1, 0)
+        self.assertTrue(math.isnan(operations_manager.perform_division()))
+
+    def test_divide_with_not_a_float(self):
+        operations_manager = dodatak_A.OperationsManager(1, "")
+        self.assertTrue(math.isnan(operations_manager.perform_division()))
+
+    def test_divide_with_not_a_number(self):
+        operations_manager = dodatak_A.OperationsManager(1, None)
+        self.assertTrue(math.isnan(operations_manager.perform_division()))
+
+
+if __name__ == '__main__':
+    unittest.main()
